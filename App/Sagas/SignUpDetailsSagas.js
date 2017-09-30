c21586371a65d95dc546a4a68989e8c6a322281b
nikit <nikitph@gmail.com>
2017-09-30 14:23:17 -0700

    photo upload challenge

--- App/Sagas/SignUpDetailsSagas.js
+++ App/Sagas/SignUpDetailsSagas.js
@@ -1,25 +1,21 @@
 import { call, put } from 'redux-saga/effects'
 import SignUpDetailsActions from '../Redux/SignUpDetailsRedux'
 import { dbService, mapp } from '../Services/Firebase'
-import { eventChannel, takeEvery } from 'redux-saga'
 import {Platform } from 'react-native'
 import { fileUpload } from '../Services/Uploader'
 
 const storage = mapp.storage();
-const database = mapp.database();
 const usr = mapp.auth();
 
 
 export function * uploadSaga ({ image, displayName, alertfunc, nav, uid}) {
   let storageRef = storage.ref(`user-images/${uid}`);
-  let hostRef = database.ref(`hostEvents/abc`);
   const uploadUri = Platform.OS === 'ios' ? image.uri.replace('file://', '') : image.uri;
 
   try
   {
-    yield call(fileUpload, uploadUri,storageRef);
-    usr.currentUser.updateProfile({displayName: displayName});
-
+    const url = yield call(fileUpload, uploadUri,storageRef);
+    usr.currentUser.updateProfile({displayName: displayName, photoURL:url});
     yield put(SignUpDetailsActions.signUpDetailsSuccess({ uid : 'yup'}));
 
   }
